0b6e994b6600fd5114bef3e49a312255be8c0929
Oyetibo Olaoluwa <olaoluwaoyetibo@gmail.com>
2026-03-26 16:37:12 +0100

    Fixed cors issue

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://property-loop.netlify.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://property-loop.netlify.app',
+      'https://waitlist.propertyloop.ng',
+      'https://propertyloop.ng',
+      'https://www.propertyloop.ng',
+    ],
     credentials: true,
   });
   app.setGlobalPrefix('api');
